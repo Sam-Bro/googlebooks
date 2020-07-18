fc776fd682f102f0b742a975c6d663817db673d4
OrbmasWin <sambrostudent@gmail.com>
2020-07-18 14:45:49 +0800

    added build files

---
+++ client/build/precache-manifest.c311fe9e9b3be374d413bd53980756d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79f6e5ab1c359e97ba7",
+    "url": "/static/css/main.1d9b804b.chunk.css"
+  },
+  {
+    "revision": "e79f6e5ab1c359e97ba7",
+    "url": "/static/js/main.bba91c42.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87873099c878e9025c85",
+    "url": "/static/css/2.9c79adc3.chunk.css"
+  },
+  {
+    "revision": "87873099c878e9025c85",
+    "url": "/static/js/2.ca8cd5cf.chunk.js"
+  },
+  {
+    "revision": "fa083d416380fa9ef6d9ba8bd2687884",
+    "url": "/index.html"
+  }
+];
--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
         </Switch>
       </Container>
     </Router>
-  );
+  ); 
 }
 
 
